9d6919a73685388b0ab7d1eea9649d9a2fe21c46
PBJTP <jplypick1990@gmail.com>
2023-03-14 13:32:34 -0700

    Add analyzeArray functionality Fix test file syntax Fix test file expecteds Change test to toMatchObject

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,15 +1,45 @@
 const analyzeArray = (arr) => {
-    let average;
+    let average = 0;
+    let sortedArray = mergeSort(arr);
     let min;
     let max;
-    let length;
+
+    for (let i = 0; i < arr.length; i++) {
+        average += arr[i];
+    }
 
     return {
-        // average: ,
-        // min: ,
-        // max: ,
+        average: (average / arr.length),
+        min: sortedArray[0],
+        max: sortedArray[arr.length - 1],
         length: arr.length
     }
 }
+
+function mergeSort(array) {
+    //base case
+    if (array.length < 2) return array;
+
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+    
+    //recursion
+    return merge(mergeSort(left), mergeSort(right));
+};
+
+function merge(left, right) {
+    let sortedArray = [];
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            sortedArray.push(left.shift());
+        } else {
+            sortedArray.push(right.shift());
+        }
+    };
+
+    return [...sortedArray, ...left, ...right];
+}
+
 console.log(analyzeArray([1,2,3,4,5]))
-module.export = analyzeArray;
+module.exports = analyzeArray;
--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -1,22 +1,22 @@
 const analyzeArray = require('./analyzeArray');
 
 test('Return the average, minimum, maximum, and length of an array input, in an object', () => {
-    expect(analyzeArray([8,7,6,5,4,3,2,1]).toBe({
+    expect(analyzeArray([8,7,6,5,4,3,2,1])).toMatchObject({
         average: 4.5,
         min: 1,
         max: 8,
         length: 8
-    }));
-    expect(analyzeArray([1,8,3,4,2,6]).toBe({
+    });
+    expect(analyzeArray([1,8,3,4,2,6])).toMatchObject({
         average: 4,
         min: 1,
         max: 8,
         length:6
-    }));
-    expect(analyzeArray([535,3,1082]).toBe({
+    });
+    expect(analyzeArray([535,3,1082])).toMatchObject({
         average: 540,
         min: 3,
-        max: 1081,
+        max: 1082,
         length: 3
-    }));
-}) 
+    });
+});
