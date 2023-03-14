02b3d9865c23100f11d95d4027a63ce4e959033e
PBJTP <jplypick1990@gmail.com>
2023-03-14 12:31:46 -0700

    Add analyzeArray test Add length solution to analyzeArray

---
+++ src/analyzeArray.js
@@ -0,0 +1,15 @@
+const analyzeArray = (arr) => {
+    let average;
+    let min;
+    let max;
+    let length;
+
+    return {
+        // average: ,
+        // min: ,
+        // max: ,
+        length: arr.length
+    }
+}
+console.log(analyzeArray([1,2,3,4,5]))
+module.export = analyzeArray;
---
+++ src/analyzeArray.test.js
@@ -0,0 +1,22 @@
+const analyzeArray = require('./analyzeArray');
+
+test('Return the average, minimum, maximum, and length of an array input, in an object', () => {
+    expect(analyzeArray([8,7,6,5,4,3,2,1]).toBe({
+        average: 4.5,
+        min: 1,
+        max: 8,
+        length: 8
+    }));
+    expect(analyzeArray([1,8,3,4,2,6]).toBe({
+        average: 4,
+        min: 1,
+        max: 8,
+        length:6
+    }));
+    expect(analyzeArray([535,3,1082]).toBe({
+        average: 540,
+        min: 3,
+        max: 1081,
+        length: 3
+    }));
+}) 
